fa34e4b02829791e043b9f1998666b3d6bbf88e2
Jaehh <0124alex@naver.com>
2024-05-08 15:33:50 +0900

    Day0508

---
+++ stswork/SpringDbTest/src/main/java/spring/mysql/coffee/HomeController.java
@@ -0,0 +1,28 @@
+package spring.mysql.coffee;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	MyShopDaoInter daoInter;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		
+		List<MyShopDto> list = daoInter.getAllList();
+		
+		model.addAttribute("list", list);
+		model.addAttribute("totalCount", list.size());
+		
+		return "home";
+	}
+	
+}
---
+++ stswork/SpringDbTest/src/main/java/spring/mysql/coffee/MyShopDto.java
@@ -0,0 +1,51 @@
+package spring.mysql.coffee;
+
+public class MyShopDto {
+	
+	private String num;
+	private String sang_name;
+	private String photo;
+	private String price;
+	private String ipgoday;
+	private String writeday;
+	
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getSang_name() {
+		return sang_name;
+	}
+	public void setSang_name(String sang_name) {
+		this.sang_name = sang_name;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getIpgoday() {
+		return ipgoday;
+	}
+	public void setIpgoday(String ipgoday) {
+		this.ipgoday = ipgoday;
+	}
+	public String getWriteday() {
+		return writeday;
+	}
+	public void setWriteday(String writeday) {
+		this.writeday = writeday;
+	}
+	
+	
+	
+}
