752c64404ba61dfcf72f9035d782ca9b2a4b34b9
Jaehh <0124alex@naver.com>
2024-05-16 16:57:25 +0900

    Day0516

---
+++ sts4work/SpringBootEx1/src/main/java/com/boot/ex1/TestDto.java
@@ -0,0 +1,17 @@
+package com.boot.ex1;
+
+import lombok.Data;
+
+
+//@Setter
+//@Getter
+//@ToString
+
+//setter,getter,tostring 포함
+@Data 
+public class TestDto {
+	
+	private String name;
+	private String addr;
+	
+}
